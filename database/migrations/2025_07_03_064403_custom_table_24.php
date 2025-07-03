1926c641fe3520e502faee27b1352a3fe6d2e21c
rianasridamanik <you@example.com>
2025-07-03 14:08:23 +0700

    Jalankan migration dan pastikan tabel tersedia di database.

--- database/migrations/2025_07_03_064403_custom_table_24.php
+++ database/migrations/2025_07_03_064403_custom_table_24.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('custom_table_24', function (Blueprint $table) {
+            $table->id();
+            $table->string('field1');
+            $table->string('field2');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
